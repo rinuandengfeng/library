6e3b6a36820022310b1f5c7eeeb322aa3e2a7a83
rndf <1061747240@qq.com>
2022-12-25 22:12:46 +0800

    获取座位写入json文件

--- appoinment_seat.py
+++ appoinment_seat.py
@@ -1,61 +1,82 @@
 import datetime
+import json
+import os
 
 import requests
 from lxml import etree
 from config import global_config
 
 from log import logger
+from utils import seatid_to_json,get_seatId
 
 from exception import SeatException
 
-#图书馆预约脚本
+
+
+
+TODAY_DATE = datetime.date.today()
+# 图书馆预约脚本
 class LibrarySeat(object):
 
     def __init__(self) -> None:
         self.session = requests.session()
         self._url = global_config.get('account', 'url')
         self._synchronizer_token = self.__get_login_index()
-        self._proixes = {"http":None,"https":None}
+        self._proxies = {"http": None, "https": None}
         self.username = global_config.get('account', 'username')
         self.passwd = global_config.get('account', 'password')
         self.isLogin = False
+        self._seat_file_path = os.path.join(os.getcwd(),global_config.get('account','seat_file_path'))
+        if not os.path.exists(self._seat_file_path):
+            os.makedirs(self._seat_file_path)
 
+    # 登录
 
-    #登录
     def logins(self):
         self.__login()
         if self.isLogin:
-            print("登录成功")
+            print("登录成功。")
         else:
-            raise SeatException("登录失败")
-
+            raise SeatException("登录失败。")
     
-    #获取登录页面
-    def __get_login_index(self)->str:
+    # 获取房间和座位
+    def get_seatid_file(self):
+        try:
+            self.logins()
+            self.requests_seat()
+        except Exception as e:
+            logger.error("登录失败，请重新登录。")
+
+
+    # 获取登录页面
+    def __get_login_index(self) -> str:
         headers = {
-            "user-agent": "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+            "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+            "Connection": "Keep-alive",
         }
         url_index = self._url + '/login'
 
-        params ={
-            "targetUri":'/'
+        params = {
+            "targetUri": '/'
         }
 
         try:
-            response = self.session.get(url_index,headers=headers,params=params,timeout=(5,3))
-            tree = etree.HTML(response.txt)
-            synchronizer_token = tree.xpath('//*[@id="SYNCHRONIZER_TOKEN"]/@value')[0]
+            response = self.session.get(
+                url_index, headers=headers, params=params, timeout=(5, 3))
+            tree = etree.HTML(response.text)
+            synchronizer_token = tree.xpath(
+                '//*[@id="SYNCHRONIZER_TOKEN"]/@value')[0]
             return synchronizer_token
         except Exception as e:
             logger.error(e)
 
-
     # 登录账号
-    def __login(self):
+    def __login(self) -> None:
         url = self._url + '/auth/signIn'
         header = {
             "Content-Type": "application/x-www-form-urlencoded",
-            "user-agent": "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+            "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+            "Connection": "Keep-alive",
             "referer": "https://zuowei.hnuahe.edu.cn/login?targetUri=%2F",
         }
         data = {
@@ -73,9 +94,64 @@ class LibrarySeat(object):
         except Exception as e:
             logger.error("登录失败:", e)
 
+    # 获取预约座位页面
+    def get_map(self):
+        url = self.url + '/map'
+        header = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "referer": "https://zuowei.hnuahe.edu.cn/",
+            "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+            "Connection": "Keep-alive",
+        }
+        respones = self.session.get(url=url, headers=header, timeout=(5, 3),proxies=self._proxies)
+
+        # 获取选座位的SYNCHRONIZER_TOKEN
+        tree = etree.HTML(respones.text)
+        map_token = tree.xpath('//*[@id="SYNCHRONIZER_TOKEN"]/@value')[0]
+        return map_token
+    
+    # 获取教室号、座位id、座位号，并存入对应的教室的json文件
+    def requests_seat(self):
+        
+        url = self._url + '/mapBook/ajaxGetRooms'
+        headers = {
+            "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+            "Connection": "Keep-alive",
+            "Referer": "https://zuowei.hnuahe.edu.cn/map"
+        }
+
+        # 将三个校区每层每个教室写成一个json文件
+        for i in range(1, 4):
+            if i == 3:
+                for j in [0, 2, 3]:
+                    data = {
+                        "building": i,
+                        "floor": j,
+                        "onDate": str(TODAY_DATE)
+                    }
+                    response = self.session.get(
+                        url=url, params=data, headers=headers, timeout=(5, 3), proxies=self._proxies)
+                    content = json.loads(response.text)["rooms"]
+                    if content:
+                        for classroom in content:
+                            # 获取座位id和座位号，并转化为json
+                            get_seatId(self,classroom=classroom)
+            else:
+                for j in range(0, 6):
+                    data = {
+                        "building": i,
+                        "floor": j,
+                        "onDate": str(TODAY_DATE)
+                    }
+                    response = self.session.get(
+                        url=url, params=data, headers=headers, timeout=(5, 3), proxies=self._proxies)
+                    content = json.loads(response.text)["rooms"]
+                    if content:
+                        for classroom in content:
+                            # 获取座位id和座位号，并转化为json
+                            get_seatId(self,classroom=classroom)
+    
 
 if __name__ == '__main__':
     test = LibrarySeat()
-    test.logins()
-
-    
+    test.get_seatid_file()
--- utils.py
+++ utils.py
@@ -1,7 +1,42 @@
+from log import logger
+import json
+import datetime
+
+
+from lxml import etree
+#当天日期
+TODAY_DATE = datetime.date.today()
+
 # 分割座位号
 def seatid_to_json(raw_seat_id, raw_seat_num):
-    seat_dict ={}
-    for i in range(0,len(raw_seat_id)):
+    seat_dict = {}
+    for i in range(0, len(raw_seat_id)):
         seat_id = raw_seat_id[i].split("_")[1]
         seat_dict[seat_id] = raw_seat_num[i]
     return seat_dict
+
+# 获取座位id和座位号
+def get_seatId(library,classroom):
+    headers = {
+        "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+        "Connection": "Keep-alive",
+        "Referer": "https://zuowei.hnuahe.edu.cn/map"
+    }
+
+    seat_data = {
+        "room": classroom["id"],
+        "date": str(TODAY_DATE)
+    }
+    seat_url = "https://zuowei.hnuahe.edu.cn" + "/mapBook/getSeatsByRoom"
+
+    seat_response = library.session.get(
+        url=seat_url, params=seat_data, timeout=(5, 3), proxies=library._proxies)
+    tree = etree.HTML(seat_response.text)
+    seat_id = tree.xpath('//ul/li/@id')
+    seat_num = tree.xpath('//ul//li/div/code/text()')
+    seat_dict = seatid_to_json(seat_id, seat_num)
+    classroom["seat_dict"] = seat_dict
+    with open(library._seat_file_path + classroom["name"] + ".json", 'w', encoding="utf-8") as f:
+        json.dump(classroom, f, indent=4,
+                ensure_ascii=False)
+    logger.info(classroom["name"]+"座位ID获取成功。")
